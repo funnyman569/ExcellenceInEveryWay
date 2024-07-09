6679763bca663b34a5787d7395c6afabeed76719
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-07-09 12:37:30 -0600

    "I am seeing if this will add everythig to git hub."

--- Readjson.cs
+++ Readjson.cs
@@ -1,27 +1,12 @@
 using System.IO
 using System.Text.Json;
 
-public class Person()
-{
-    public string f_name;
-    public string l_name;
-    public string title;
-    public string email;
-    public string phoneNumber;
-    public List<string> goals = new List<string>();
-
-    public Person() { 
-        goals = new List<string>();
-    }
-}
-
 public class ReadJson
 {
     public static void Main()
     {
         string json = File.ReadAllText("jaonfile.js");
-        Person person = JsonSerializer.Deserialize<Person>(json);
-        Console.WriteLine(person.f_name);
+        
     }
 }
 
