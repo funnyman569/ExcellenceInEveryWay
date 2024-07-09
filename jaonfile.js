e748ca982471860d4311d65115a47ce8e9b6bebc
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-07-09 11:43:49 -0600

    here is the json file that we wil be pulling the information out of to keep track of our workers and said clients.

---
+++ jaonfile.js
@@ -0,0 +1,25 @@
+const worker = [
+    {
+        f_name: "Cesar",
+        l_name: "Tavarez",
+        title: "Behavior Interventionist",
+        name: "Julio Cesar Tavarez",
+        H_hourly_pay: "$19",
+        phon_number: "208-555-1234"
+    },
+    {
+        f_name: "Hayden",
+        l_name: "Harris",
+        title: "Behavior Interventionist",
+        name: "Julio Cesar Tavarez",
+        H_hourly_pay: "$19",
+        phon_number: "208-555-1234"
+    }
+]
+const client = [
+    {
+        name: "Brayden Bold",
+        goals: ['wash hand', 'play nice with others', 'eat healthy']
+    }
+
+]
