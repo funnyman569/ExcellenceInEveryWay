46d38eeb5913cf3068fe6fb73f48cc846eb85458
Hayden Harris <haydenandamyharris@gmail.com>
2024-07-10 09:25:20 -0600

    fixing

---
+++ Deserializer.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using System.Text.Json;
+class Deserializer
+{
+    public string f_name;
+    public string l_name;
+    public string title;
+    public string H_hourly_pay;
+    public string phon_number;
+    public string name;
+    public List<string> Goals;
+
+    public ReadJson
+    {
+
+    }
+}
