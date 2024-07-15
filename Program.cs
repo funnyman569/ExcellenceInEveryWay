babaccc830b0b533eb707e5cc083eb9b9cd1f887
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-07-15 05:36:52 -0600

    changing the json file

--- Program.cs
+++ Program.cs
@@ -2,6 +2,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Pick a number between 1 and 3 ");
+        console.WriteLine("Welcome to the Excellence in Every Way. Please choose an option.");
+        console.WriteLine("1. Fill out Form for the day.");
+        console.WriteLine("2. Schedule times for the day.");
+        console.WriteLine("3. Send form to insurance companies and gov");
+        console.WriteLine("4. Pay Emplayees");
+        console.WriteLine("5. Create new employee");
+        console.writelie("6. Create new client");
+        console.WriteLine("7. Exit");
+        console.Write("Pick a number between 1 and 5: ");
+        int choice = int.Parse(console.ReadLine());
+
+        if (choice == 1)
+        {
+            FillOutForm();
+        }
+        else if (choice == 2)
+        {
+            ScheduleTimes();
+        }
+        else if (choice == 3)
+        {
+            SendForm();
+        }
+        else if (choice == 4)
+        {
+            PayEmployees();
+        }
+        else if (choice == 5)
+        {
+
+        }
+        else if (choice == 6)
+        {
+            CreateNewClient();
+        }
+        else if (choice == 7)
+        {
+            Exit();
+        }
+        else
+        {
+            console.WriteLine("Invalid choice.");
+            Main(args);
+        }
     }
 }
--- jaonfile.js
+++ jaonfile.js
@@ -5,14 +5,15 @@ const workers = [
         l_name: "Tavarez",
         title: "Behavior Interventionist",
         hourly_pay: "$19",
-        phon_number: "208-555-1234"
+        phon_number: "208-555-1234",
+        email: "tav23002@gmail.com"
     },
     {
         f_name: "Hayden",
         l_name: "Harris",
         title: "Behavior Interventionist",
-        name: "Julio Cesar Tavarez",
         hourly_pay: "$19",
-        phon_number: "208-555-1234"
+        phon_number: "208-555-1234",
+        email: "HaHrr@example.com"
     }
 ]
