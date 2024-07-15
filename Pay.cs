a7d9627bde7100741db931f7fe0022a3ad422d6e
Hayden Harris <haydenandamyharris@gmail.com>
2024-07-15 09:07:49 -0600

    Added Updates for Pay class, Still need to adjust all setters

--- Pay.cs
+++ Pay.cs
@@ -1,4 +1,18 @@
-class Pay
+class Pay : Worker
 {
-    
+    private int hourlyPay
+
+    public Pay : base()
+    {
+        hourlyPay = 0
+    }
+
+   
+   //Need to add 
+    public int SetHourlyPay
+    {
+
+        hourlyPay =  
+    }
+
 }
