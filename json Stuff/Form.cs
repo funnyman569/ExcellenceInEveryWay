c39d87c559e3ac34015a03c74d7527caf67bb04c
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-07-15 09:57:13 -0600

    i am trying to hook up my git hub

--- json Stuff/Form.cs
+++ json Stuff/Form.cs
@@ -2,36 +2,51 @@ class Form
 {
     public static Form()
     {
-        string date = console.WriteLine("Date: " + DateTime.Now);
+        string date = DateTime.Now.ToString("yyyy-MM-dd");
+        console.WriteLine("Date: " + date);
+
         console.Write("Worker's First Name: ");
         string workerFirstName = console.ReadLine();
+
         console.Write("Worker's Last Name: ");
         string workerLastName = console.ReadLine();
+
         console.write("JobTitle: ");
         string jobTitle = console.ReadLine();
+
         console.Write("StartTime: ");
         string startTime = console.ReadLine();
+
         console.Write("EndTime: ");
         string endTime = console.ReadLine();
+
         console.Write("Total Hours: ");
         string totalHours = console.ReadLine();
+
         console.WriteLine("Client Info: ");
         console.Write("First Name: ");
         string c_firstName = console.ReadLine();
+
         console.Write("Last Name: ");
         string c_lastName = console.ReadLine();
+
         Client_info client = new Client_info();
         client.getClientInfo(c_firstName, c_lastName);
-        foreach (string goal in client.goals)
+        if (client != null)
         {
-            console.WriteLine($"Goal: {goal}"); 
-            console.Write("How good did they do this Goal [1-5]: ")
-            string c_goalRating = console.ReadLine();
-            console.WriteLine("Notes: ");
-            string c_notes = console.ReadLine();
+            foreach (string goal in client.goals)
+            {
+                console.WriteLine($"Goal: {goal}"); 
+                console.Write("How well did they do on this Goal [1-5]: ")
+                string c_goalRating = console.ReadLine();
+                console.WriteLine("Notes: ");
+                string c_notes = console.ReadLine();
+            }
         }
-
-
+        else
+        {
+            console.WriteLine("Client not found");
+        }
 
     }
 }
--- json Stuff/jaonfile.js
+++ json Stuff/jaonfile.js
@@ -17,3 +17,4 @@ const workers = [
         hourly_pay: "19",
     }
 ]
+
