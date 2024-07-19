c035d9e0132991913146407edafdbbe170b5bd1b
Hayden Harris <haydenandamyharris@gmail.com>
2024-07-18 20:27:46 -0600

    yea

--- Pay.cs
+++ Pay.cs
@@ -1,23 +1,56 @@
 
 class Pay : Worker
 {
-    private string hourlyPay{get; set;}
-    private int hoursWorked{get;}
+    private int hourlyPay{get; set;}
+    private int hoursWorked;
+    private int totalPay;
+     List<Worker> listOfWorkers = new();
 
-    public Pay()
-    {
-        hoursWorked = 5;
+    public Pay() : base()
+    {   
+        hoursWorked = 0;
+        totalPay = 0;
+        hourlyPay = 0;
     }
 
-    public int SetTotalPay(int hourlyPay, int hoursWorked)
+    public void CalculateAndPrintTotalPays()
     {
-       return hourlyPay * hoursWorked;
+        listOfWorkers = CreateInstanceofWorker();
+        foreach (Worker person in listOfWorkers)
+        {
+            System.Console.WriteLine($"How many hours did {person.f_name} work?");
+            SetHoursWorked(int.Parse(Console.ReadLine()));
+            if (int.TryParse(person.hourly_pay, out int parsedHourlyPay))
+            {
+                SetTotalPay(parsedHourlyPay, hoursWorked);
+                 Console.WriteLine($"Total Pay for {person.f_name} {person.l_name}: {GetTotalPay()}");
+            }
+            
+        }
     }
 
-public void PrintTotalPay()
-{
-    System.Console.WriteLine(SetTotalPay(int.Parse(hourlyPay), hoursWorked ));
-}
+    public void SetHoursWorked(int HoursWorked)
+    {
+        hoursWorked = HoursWorked;
+    }
+    public int GetHourlyPay()
+    {
+        return hourlyPay;
+    }
+
+    public int GetHoursWorked()
+    {
+        return hoursWorked;
+    }
+    public void SetTotalPay(int hourlyPay, int hoursWorked)
+    {
+       totalPay = hourlyPay * hoursWorked;
+    }
+
+    public int GetTotalPay()
+    {
+        return totalPay;
+    }
 
 
 
